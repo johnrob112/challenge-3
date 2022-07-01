dab7268d7e3845f0f90a4278cb016cfa08b1c73d
John Robinson <jxr1372@miami.edu>
2022-06-30 22:09:30 -0400

    close to final

--- Develop/script.js
+++ Develop/script.js
@@ -8,166 +8,83 @@
 
 // Character arrays
 
-var special = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~' ];
-var lowercase = [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-var uppercase = [ 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-var numeric = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialArr = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~' ];
+var lowercaseArr = [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+var uppercaseArr = [ 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+var numericArr = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-// Variables for different combinations of characters 
+// Insert blank array to account for the differnet possible combinations of characters
 
-var allCharacters = special + lowercase + uppercase + numeric;
-var lu = lowercase + uppercase;
-var uln = lowercase + uppercase + numeric;
-var un = uppercase + numeric;
-var ln = lowercase + numeric;
-var sl = special + lowercase;
-var su = special + uppercase;
-var sn = special + numeric;
-var slu = special + lowercase + uppercase;
-var sun = special + uppercase +numeric;
-var sln = special + lowercase + numeric;
+var comboArrays = [];
 
-// Blank string for new password 
-
-
-
-// Function to select password criteria
-
-function selectPassword(){
-
-let lengthChoice = prompt ("Enter the number of characters you would like your password to be (Must be between 8-128)");
-if (isNaN(lengthChoice) || lengthChoice <8 || lengthChoice > 128) {
-  lengthChoice = prompt("Please enter a number from 8 to 128 and try again.");
-}
-
-let specialSelect = confirm("Include Special Characters? (!, #, %, &, *, etc)");
-let lowerSelect = confirm("Include lowecase characters?");
-let upperSelect = confirm("Include uppercase characters?");
-let numericSelect = confirm ("Include numeric characters? (0-9)");
-
-
-
-
-}
-
-// Create if statements with loops to create the password combination
-
-function passwordCombination() {
-
-  
-  if ((specialSelect === true) && (lowerSelect === true) && (upperSelect === true) && (numericSelect === true)) {
-    for (i = 0; i < lengthChoice; i++) {
-        let character = Math.floor(Math.random() * allCharacters.length);
-        newPassword += allCharacters.charAt(character, character + 1);
-    }
-    return;
-}
-
-else if ((specialSelect === false) && (lowerSelect === true) && (upperSelect === true) && (numericSelect === false)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * lu.length);
-      newPassword += lu.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === false) && (lowerSelect === true) && (upperSelect === true) && (numericSelect === true)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * uln.length);
-      newPassword += uln.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === false) && (lowerSelect === false) && (upperSelect === true) && (numericSelect === true)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * un.length);
-      newPassword += un.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === false) && (lowerSelect === true) && (upperSelect === false) && (numericSelect === true)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * ln.length);
-      newPassword += ln.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === true) && (lowerSelect === true) && (upperSelect === false) && (numericSelect === false)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * sl.length);
-      newPassword += sl.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === true) && (lowerSelect === false) && (upperSelect === true) && (numericSelect === false)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * su.length);
-      newPassword += su.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === true) && (lowerSelect === false) && (upperSelect === false) && (numericSelect === true)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * sn.length);
-      newPassword += sn.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === true) && (lowerSelect === true) && (upperSelect === true) && (numericSelect === false)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * slu.length);
-      newPassword += slu.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === true) && (lowerSelect === false) && (upperSelect === true) && (numericSelect === true)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * sun.length);
-      newPassword += sun.charAt(character, character + 1);
-  }
-}
-
-else if ((specialSelect === true) && (lowerSelect === true) && (upperSelect === false) && (numericSelect === true)) {
-  for (i = 0; i < lengthChoice; i++) {
-      let character = Math.floor(Math.random() * sln.length);
-      newPassword += sln.charAt(character, character + 1);
-  }
-}
-
-return newPassword;
-
-
-}
-
-
-
-
-
-function generatePassword() {
-
-  var password = "";
-  var selection = selectPassword();
-  var combination = passwordCombination();
-
-  return password; 
-  
-
-
-}
-
-
-// Get references to the #generate element
+// Assignment code here
 var generateBtn = document.querySelector("#generate");
 
+//Add Event Listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write function to allow the user to select their password criteris through a series of prompts.
+
+function passwordCriteria() {
+
+  lengthChoice = parseInt(prompt("Please enter the amount of characters you would like your password to be (between 8 and 128)"));
+  if(isNaN(lengthChoice) || lengthChoice < 8 || lengthChoice > 128) {
+    alert("Password must be between 8-128. Please try again.");
+    return false;
+  }
+
+  // Use concat to join more than one strings.
+
+
+  if (confirm("Would you like lowercase letters in your password?")) {
+      comboArrays = comboArrays.concat(lowercaseArr);
+    }
+
+    // console.log (comboArrays); (used to make sure comboArrays is being filled correctly)
+
+
+  if (confirm("Would you like uppercase letters in your password?")) {
+      comboArrays = comboArrays.concat(uppercaseArr);
+    }
+
+    // console.log (comboArrays);
+
+  if (confirm("Would you like numbers in your password?")) {
+      comboArrays = comboArrays.concat(numericArr); 
+    }
+
+    // console.log (comboArrays);
+
+
+  if (confirm("Would you like special characters in your password?")) {
+      comboArrays = comboArrays.concat(specialArr);
+    }
+
+    // console.log (comboArrays);
+
+    return true;
+}
+
+// Write function to generate the user's new password based on their answers to the prompts.
+
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < lengthChoice; i++) {
+    var randomIndex = Math.floor(Math.random() * comboArrays.length)
+    password = password + comboArrays[randomIndex];
+  }
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var criteria = passwordCriteria();
   var passwordText = document.querySelector("#password");
-
+  if (criteria) {
+  var password = generatePassword();
   passwordText.value = password;
-
+  } else {
+  passwordText.value = "";
+  }
+  
 }
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
